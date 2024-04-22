f68083fe15a970533b709853f736608bee42f050
Eerik Poopuu <91658707+Eerikpoopuu@users.noreply.github.com>
2024-04-22 21:00:15 +0300

    kool

---
+++ mosh/src/Main.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.util.Arrays;
+import java.util.Date;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int[] nrs = {2,7,4,5,6};
+        Arrays.sort(nrs);
+        System.out.println(Arrays.toString(nrs));
+
+    }
+    }
