67fa469795c244c12d11c06bf12e53540cbc5e15
Eerik Poopuu <91658707+Eerikpoopuu@users.noreply.github.com>
2024-05-07 15:56:53 +0300

    kool

--- 13_kontrolltoo2/kontrolltoo/src/main/java/ee/tlu/kontrolltoo/OmanikRepository.java
+++ 13_kontrolltoo2/kontrolltoo/src/main/java/ee/tlu/kontrolltoo/OmanikRepository.java
@@ -1,6 +1,11 @@
-package ee.tlu.kontrolltoo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OmanikRepository extends JpaRepository<Omanik, Long> {
-}
+//package ee.tlu.kontrolltoo;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.List;
+//
+//public interface OmanikRepository extends JpaRepository<Omanik, Long> {
+//    Omanik findTopByOmanikNimiOrderByPikkusDesc(String nimi);
+//
+//    List<Omanik> findByOmanikNimi(String nimi);
+//}
